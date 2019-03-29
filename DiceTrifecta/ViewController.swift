a4d0bf0c6c7d7a27eddff0c4b65e081c644d992e
Robert Lent <lentcoding@outlook.com>
2019-03-29 00:20:11 -0700

    Game V1

--- DiceTrifecta/ViewController.swift
+++ DiceTrifecta/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dice1View: UIImageView!
+    @IBOutlet weak var dice2View: UIImageView!
+    @IBOutlet weak var dice3View: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    let defaults = UserDefaults.standard
+    
+    var dice1 = 6
+    var dice2 = 6
+    var dice3 = 6
+    var score = 0
+    var highScore = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        newGame()
+    }
+    
+    func newGame() {
+        score = 0
+        highScore = defaults.integer(forKey: "highScore")
+        
+        scoreLabel.text = "Score: \(score)"
+        highScoreLabel.text = "High Score: \(highScore)"
+    }
+    
+    func updateScore(points: Int) {
+        score += points
+        scoreLabel.text = "Score: \(score)"
+        
+        if score > highScore {
+            highScore = score
+            defaults.set(highScore, forKey: "highScore")
+            highScoreLabel.text = "High Score: \(highScore)"
+        }
     }
 
-
+    @IBAction func rollDice(_ sender: Any) {
+        dice1 = Int.random(in: 1...6)
+        dice2 = Int.random(in: 1...6)
+        dice3 = Int.random(in: 1...6)
+        
+        dice1View.image = UIImage(named: "dice-\(dice1)")
+        dice2View.image = UIImage(named: "dice-\(dice2)")
+        dice3View.image = UIImage(named: "dice-\(dice3)")
+        
+        if dice1 == dice2 && dice1 == dice3 {
+            updateScore(points: 100 * dice1)
+        } else if dice1 == dice2 || dice1 == dice3 || dice2 == dice3 {
+            updateScore(points: 50)
+        }
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,14 @@
+# Dice Trifecta
+Dice Trifecta is an iOS game written in Swift where you roll three dice, trying to get as high a score as possible.
+
+If you roll three dice of the same value (a Trifecta), your score will increase by that value times 100, i.e. If you roll 6's, you will get 600 points.
+
+If you roll two dice of the same value, you will get 50 points.
+
+You receive no points if each dice has a different value.
+
+### Screenshot
+![ScreenShot](Screenshots/AppInterface.png)
+
+### Works on
+The app is written in Swift 4, and should run on most devices running iOS 9 or later.
